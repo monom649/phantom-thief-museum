445eea323c7e4cc422a1ab19938abf9a3589332e
尾野充 <mitsuruono@onomMacBook-Pro.local>
2025-08-13 00:15:25 +0900

    update: constants.ts

--- constants.ts
+++ constants.ts
@@ -1,10 +1,11 @@
+
 import { おもちゃ } from './types';
 
 // ゲーム設定
 export const 制限時間 = 120; // 2分（秒）
 export const おもちゃの総数 = 6;
-export const サンサン振り向き間隔_最小 = 500; // 0.5秒 
-export const サンサン振り向き間隔_最大 = 1500; // 1.5秒
+export const サンサン振り向き間隔_最小 = 100; // 0.1秒
+export const サンサン振り向き間隔_最大 = 500; // 0.5秒
 
 // 画像・音声アセット
 export const アセット = {
@@ -17,21 +18,21 @@ export const アセット = {
   サンサン右: 'https://i.imgur.com/DM7mJnP.png', // サンサンが右を向く
   サンサン左: 'https://i.imgur.com/WES5Mm0.png', // サンサンが左を向く
   ノイズ待機: 'https://i.imgur.com/4Q7Fj1L.png',
-  ノイズ盗む左: 'https://i.imgur.com/e6iNXXP.png',
-  ノイズ盗む右: 'https://i.imgur.com/YuphH0M.png',
+  ノイズ盗む左: 'https://i.imgur.com/e6iNXXP.png', // ノイズが左に手を伸ばす
+  ノイズ盗む右: 'https://i.imgur.com/YuphH0M.png', // ノイズが右に手を伸ばす
   
   // UIボタン
   ボタン左: 'https://i.imgur.com/JBLP1sK.png',
   ボタン盗む: 'https://i.imgur.com/Wriattw.png',
   ボタン右: 'https://i.imgur.com/XYCX8Xz.png',
-  
-  // 音声（local public/直下）
-  スタート効果音: '/リアクション_ノイズ016.wav',
-  移動音右: '/リアクション_ノイズ024.wav',
-  移動音左: '/リアクション_ノイズ025.wav',
-  盗む音: '/リアクション_ノイズ029.wav',
-  ゲームオーバー音: '/リアクション_ノイズ039.wav',
-  クリア音: '/リアクション_ノイズ046.wav',
+
+  // 音声
+  スタート効果音: '/リアクション_ノイズ016.wav',
+  移動音右: '/リアクション_ノイズ024.wav',
+  移動音左: '/リアクション_ノイズ025.wav',
+  盗む音: '/リアクション_ノイズ029.wav',
+  ゲームオーバー音: '/リアクション_ノイズ039.wav',
+  クリア音: '/リアクション_ノイズ046.wav',
 };
 
 export const すべてのおもちゃ: おもちゃ[] = [
@@ -42,4 +43,3 @@ export const すべてのおもちゃ: おもちゃ[] = [
   { id: 5, 名前: '恐竜', 画像URL: 'https://i.imgur.com/ppiqTXe.png', 配置: 'right' },
   { id: 6, 名前: '消防車ロボ', 画像URL: 'https://i.imgur.com/2LbWYZI.png', 配置: 'right' },
 ];
-
